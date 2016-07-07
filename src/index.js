77f3b6f7d06cae67f84962a825fbe3dd2737aed1
lakb248 <lakb248@163.com>
2016-07-07 19:38:00 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+var router = new VueRouter();
+
+var Pager = Vue.extend({
+    data() {
+        return {};
+    }
+});
+
+router.start(Pager, document.body);
