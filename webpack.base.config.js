8c59feae2e3fc9035eac958071af3dd61ddec672
lakb248 <lakb248@163.com>
2016-07-19 12:54:03 +0800

    init project

---
+++ webpack.base.config.js
@@ -0,0 +1,40 @@
+/**
+ * @file webpack base config file
+ */
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: {
+        build: './src/index.js',
+        vendor: ['vue', 'vue-resource', 'vue-router']
+    },
+    output: {
+        path: require('path').resolve('build') + '/',
+        publicPath: '/build/',
+        filename: 'build.js',
+        chunkFilename: '[hash].[name].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        }, {
+            test: /\.vue$/,
+            loader: 'vue'
+        }, {
+            test: /\.scss$/,
+
+            loader: ExtractTextPlugin.extract({notExtractLoader: 'style', loader: 'css!sass'}),
+            exclude: /node_modules/
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'url-loader?limit=8192'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({name:'vendor', filename:'vendor.bundle.js'}),
+        new ExtractTextPlugin('index.css')
+    ]
+};
